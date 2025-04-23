f273a631d3bee7302b23897640509031e7f936bc
Vivek Kumar <vivekgupta70509@gmail.com>
2025-04-23 23:44:05 +0530

    Add files via upload

---
+++ Assignment17.py
@@ -0,0 +1,41 @@
+"""
+#Q1
+n=int(input("print value of n"))
+i=1
+while i<=n:
+    print("MysirG")
+    i+=1
+print()
+
+
+#Q2
+n=int(input("pirnt value of n"))
+i=1
+while i<=n:
+    print(i)
+    i+=1
+
+    
+
+#Q3
+n=int(input("print value of n "))
+i=n
+while i>0:
+    print(i)
+    i-=1
+
+#Q4
+n=int(input("pirnt value of n"))
+i=1
+while i<=n:
+    print(2*i-1)
+    i+=1
+
+"""
+
+#Q5
+n=int(input("print value of n "))
+i=n
+while i>0:
+    print(2*i-1)
+    i-=1
